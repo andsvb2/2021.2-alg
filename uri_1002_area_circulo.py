ff7fa1533efcf90340e4b0c733ce7e087a443495
Anderson S. Vieira <svieira.anderson@gmail.com>
2021-10-13 10:31:42 -0300

    muda o momento da conversão do input para float

--- uri_1002_area_circulo.py
+++ uri_1002_area_circulo.py
@@ -5,8 +5,8 @@
 # Valores teste: 2.00, 100.64, 150.00
 
 n = 3.14159
-raio = input()
-area = n * (float(raio) ** 2)
+raio = float(input())
+area = n * (raio ** 2)
 
 # print("A=%.4f" % (area))        # String.format()
 print(f"A={area:.4f}")          # f-strings
