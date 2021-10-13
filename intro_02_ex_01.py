975fa51a223d0c09079296be09cfbf116fba2548
Anderson S. Vieira <svieira.anderson@gmail.com>
2021-10-13 17:39:38 -0300

    insere resposta do slide

--- intro_02_ex_01.py
+++ intro_02_ex_01.py
@@ -21,3 +21,21 @@ print(f"b) O resto da divisão de {num_a} por {num_b} é {div_int}.")
 # c) O correspondente a 35 % do valor de B
 porcentagem = 0.35 * num_b
 print(f"c) {porcentagem:.2f} é equivalente a 35% de {num_b}.")
+
+
+# Resposta fornecida no slide
+#
+# A = int(input("Digite o número:"))
+# B = int(input("Digite o número:"))
+#
+# #a)
+# parteInteira = A // B
+# print("Resultado Parte Inteira: %d" %parteInteira)
+#
+# #b)
+# resto = A % B
+# print("Resultado Resto: %d" %resto)
+#
+# #xc)
+# porcentagem = B * 0.35
+# print("Resultado Porcentagem : %.2f" %porcentagem)
