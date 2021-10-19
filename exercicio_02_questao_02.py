956f8f69c937f55af2ef131fe1715c2e93931c5a
Anderson S. Vieira <svieira.anderson@gmail.com>
2021-10-19 19:47:11 -0300

    cria programa retorna situacao eleitoral

---
+++ exercicio_02_questao_02.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+idade = int(input("Idade: "))
+
+if idade < 16:
+    print("Não eleitor.")
+elif 18 > idade >= 16 or idade >= 65:
+    print("Eleitor facultativo.")
+else:
+    print("Eleitor obrigatório.")
+    if idade == 18:
+        sexo = input("Informe seu sexo, digitando M ou F: ")
+        if sexo == "M" or sexo == "m":
+            print("Lembre do seu Alistamento Militar.")
