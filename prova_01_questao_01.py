4462b6b49e66abe149928604730a584298519a72
Anderson S. Vieira <svieira.anderson@gmail.com>
2021-10-20 18:51:20 -0300

    primeira questão da prova

---
+++ prova_01_questao_01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+PI = 3.14
+raio = float(input("Informe o valor do raio: "))
+
+print(f"O comprimento da circunferência é {2 * PI * raio:.2f}.",
+      f"A área do círculo é {PI * raio ** 2:.2f}.",
+      sep = "\n")
