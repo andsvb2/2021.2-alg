4c6d08858982e52879586c2dd8b7300a3eb8ee86
Anderson S. Vieira <svieira.anderson@gmail.com>
2021-11-15 15:29:44 -0300

    acrescenta solucao do augusto (debug)

--- URI/uri_1146_sequencias_crescentes.py
+++ URI/uri_1146_sequencias_crescentes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+# Solução criada por mim
 while True:
     y,z = str(),str()
     x = int(input())
@@ -10,3 +11,16 @@ while True:
             z = str(i)
             y += z + " "
     print(y.rstrip())
+
+# # Solução criada pelo Augusto
+# x = int(input())
+# lista = []
+# while(x != 0):
+#     x = x + 1
+#     for a in range(1,x):
+#         a = str(a)
+#         lista.append(a)
+#     numeros = " ".join(lista)
+#     print(numeros)
+#     lista = []
+#     x = int(input())
