56d0c239c74be8512b87a07da036cbbbab082ea3
Anderson S. Vieira <svieira.anderson@gmail.com>
2021-11-15 18:43:24 -0300

    adiciona solucao criada pelo augusto

--- URI/uri_1177_preencimento_vetor_2.py
+++ URI/uri_1177_preencimento_vetor_2.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 
+# Solução que criei
 x = int(input())
-
 contador = 0
-
 while contador < 1000:
     for num in range(x):
         print(f"N[{contador}] = {num}")
         contador += 1
         if contador == 1000:
             break
+
+
+# # Solução do Augusto Almeida
+# lista = []
+# n = int(input())
+# for a in range(1000):
+#     for b in range(n):
+#         lista.append(b)
+#     print(f"N[{a}] = {lista[a]}")
