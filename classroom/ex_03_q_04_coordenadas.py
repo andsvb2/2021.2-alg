f1cb2d31f99b180b586fc7046c2db9bfd8b3d498
Anderson S. Vieira <svieira.anderson@gmail.com>
2021-11-24 18:07:14 -0300

    atualiza questao coordenadas

--- classroom/ex_03_q_04_coordenadas.py
+++ classroom/ex_03_q_04_coordenadas.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
 
-import re
-
-while True:
-    entrada = input("Insira as coordenadas do ponto.\nQuando quiser parar o programa, digite 0 (zero) em uma das coordenadas.\n\t")
-    if re.search(',',entrada):
-        x,y = [float(i) for i in entrada.split(',')]
-    elif re.search(' ', entrada):
-        x,y = [float(i) for i in entrada.split()]
-
-    if x == 0 or y == 0:
-        print("Uma das coordenadas é zero.")
-        break
-    elif x > 0.0:
-        if y > 0.0:
+def encontrar_coordenada(prim,seg):
+    if prim > 0.0:
+        if seg > 0.0:
             local = "Q1"
         else:
             local = "Q4"
     else:
-        if y > 0.0:
+        if seg > 0.0:
             local = "Q2"
         else:
             local = "Q3"
-    print(local)
+    print(f"\nA coordenada está no quadrante {local}.\n")
+
+while True:
+    x = float(input("Insira a coordenada X: "))
+    y = float(input("Insira a coordenada Y: "))
+
+    if x == 0 or y ==0:
+        print("Uma das coordenadas é zero.\nPrograma finalizado.")
+        break
+    else:
+        encontrar_coordenada(x,y)
