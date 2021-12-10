cca27a0be95e40ee1a6d4d7f55ef50a2be47e494
Anderson S. Vieira <svieira.anderson@gmail.com>
2021-12-10 19:26:11 -0300

    ajusta as perguntas

--- classroom/ex_05_q_01_potencia.py
+++ classroom/ex_05_q_01_potencia.py
@@ -3,7 +3,7 @@
 def potencia(prim,seg):
     return prim ** seg
 
-x = int(input("Digite um número inteiro, que será a base da potenciação: "))
-y = int(input("Digite outro número inteiro, que será o expoente da potenciação: "))
+x = int(input("Qual número inteiro será a base da potenciação? "))
+y = int(input("Qual número inteiro será o expoente da potenciação? "))
 
-print(f"O resultado da potenciação é: {potencia(x,y)}.")
+print(f"O resultado da potenciação é {potencia(x,y)}.")
