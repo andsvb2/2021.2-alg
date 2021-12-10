c252cc1c1b8fb3c70e46512e4904e4fa6782fa9f
Anderson S. Vieira <svieira.anderson@gmail.com>
2021-12-10 19:22:38 -0300

    ajusta formatação

--- classroom/ex_05_q_02_fibonacci.py
+++ classroom/ex_05_q_02_fibonacci.py
@@ -8,11 +8,8 @@ def fibonacci(limite):
             seq.append(novo_valor)
     elif limite == 1:       # Mantem uma lista sendo retornada, mas apenas com um número
         seq.pop()
-
     return seq
 
 qtd = int(input("Qual o limite desejado para a sequência? ")) # alterado seguindo dica da Marina, com base em princípios de UX writing
-
 lista = fibonacci(qtd)
-
 print(f"Sequência Fibonacci com {qtd} número: {lista}") if len(lista) == 1 else print(f"Sequência Fibonacci com os {qtd} primeiros números: {lista}")
