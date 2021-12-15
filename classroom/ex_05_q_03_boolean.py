2063e04cc30e15f0620f08004b1b32ae933fe8f2
Anderson S. Vieira <svieira.anderson@gmail.com>
2021-12-15 17:08:58 -0300

    corrige os problemas da questão

--- classroom/ex_05_q_03_boolean.py
+++ classroom/ex_05_q_03_boolean.py
@@ -1,35 +1,18 @@
 #!/usr/bin/env python3
 
-# import math
+import math
 
-def verifica_num_primo(num):
-    if x in [2,3,5,7,11]:
+def verifica_primo(num):
+    if num < 2:
+        return False
+    elif num == 2 or num == 3:
         return True
-    elif x % 2 == 0:
-        return False
-    # A construção de sum abaixo é uma ideia retirada do link
-    # https://www.kite.com/python/answers/how-to-sum-the-digits-of-a-number-in-python
-    # elif sum(int(i) for i in str(x)) % 3 == 0:
-    elif x % 3 == 0:
-        return False
-    elif x % 5 == 0:
-        return False
-    elif x % 7 == 0:
-        return False
     else:
-        return True
-
-    if num > 1:
-        # fórmula adaptada de https://www.programiz.com/python-programming/examples/prime-number
-        for i in range(2,num):#math.floor(math.sqrt(num)+1)):
+        for i in range(2,math.floor(math.sqrt(num)+1)):
             if (num % i) == 0:
                 return False
-                break
-            else:
-                return True
-    else:
-        return False
+        return True
 
+entrada = int(input("Qual número gostaria de verificar? "))
 
-num = int(input("Qual número gostaria de verificar? "))
-print(f"{num} é um número primo.") if verifica_num_primo(num) is True else print(f"{num} não é um número primo.")
+print(f"{entrada} é um número primo.") if verifica_primo(entrada) is True else print(f"{entrada} não é um número primo.")
