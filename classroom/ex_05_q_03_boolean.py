34a9990f3225b755d17a64190b6416435d16a236
Anderson S. Vieira <svieira.anderson@gmail.com>
2021-12-15 23:17:53 -0300

    resolve a questão

--- classroom/ex_05_q_03_boolean.py
+++ classroom/ex_05_q_03_boolean.py
@@ -5,8 +5,6 @@ import math
 def verifica_primo(num):
     if num < 2:
         return False
-    elif num == 2 or num == 3:
-        return True
     else:
         for i in range(2,math.floor(math.sqrt(num)+1)):
             if (num % i) == 0:
